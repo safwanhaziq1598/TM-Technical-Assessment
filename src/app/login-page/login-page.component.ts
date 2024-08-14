a021a586eedc28421fe7fb44f40ddaaca67bd776
unknown <safwan.haziq1598@gmail.com>
2024-08-14 08:56:00 +0800

    feat(login-page): add angular material(mat-card)

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 
 //Angular Material
 import { MatCardModule } from '@angular/material/card';
-
+import { MatInput } from '@angular/material/input';
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatInput
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'

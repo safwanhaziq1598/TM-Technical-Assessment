b16d38aadf35ad70ea534e88030c9008d86e6194
unknown <safwan.haziq1598@gmail.com>
2024-08-15 22:56:19 +0800

    feat(add-product-dialog): Add component add-product dialog

---
+++ src/app/add-product-dialog/add-product-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-product-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-product-dialog.component.html',
+  styleUrl: './add-product-dialog.component.scss'
+})
+export class AddProductDialogComponent {
+
+}

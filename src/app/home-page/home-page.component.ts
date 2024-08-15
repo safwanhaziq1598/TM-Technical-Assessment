caa2859c21b2c45de96c5ccaa7e31f9323f2ccce
unknown <safwan.haziq1598@gmail.com>
2024-08-16 00:08:31 +0800

    Add testing function for product data (cont.)

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,31 +6,54 @@ import { productDetails } from '../../models/product';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent implements OnInit {
   tableData: productDetails[] = [];
-
+  productId: string = "";
+  token: string = "";
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
-    const token = localStorage.getItem('token')!;
+    this.getProductListData();
+  }
+
+  getProductListData() {
+    this.token = localStorage.getItem('token')!;
 
     try {
-
-      this.productService.getProductList(token).subscribe((data) => {
-        console.log(data);
-        this.tableData = data;
-      },
-    (error) => {
-      console.error('Error fetching product list: ', error);
-    });
+      this.productService.getProductList(this.token).subscribe(
+        (data) => {
+          console.log(data);
+          this.tableData = data;
+          this.productId = data.id;
+          this.getProductData(this.token, this.productId);
+        },
+        (error) => {
+          console.error('Error fetching product list: ', error);
+        }
+      );
     } catch (error) {
       console.error(error);
+    }
+  }
+
+  getProductData(token: string, id: string) {
+    try {
+      this.productService.getProductDetail(token, id).subscribe(
+        (data) => {
+          console.log(data)
+        },
+        (error) => {
+          console.error('Error fetching product data: ', error);
+        }
+      )
+
+    } catch (error) {
+      console.error(error);
+
     }
   }
 }
--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Params } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private apiUrl = 'https://intermediate-test-v-2-web-test.apps.ocp.tmrnd.com.my/api/data/productList';
-
+  private apiUrl1 = 'https://intermediate-test-v-2-web-test.apps.ocp.tmrnd.com.my/api/data/productList';
+  private apiUrl2 = 'https://intermediate-test-v-2-web-test.apps.ocp.tmrnd.com.my/api/data/alert/list'
   constructor(
     private http: HttpClient
   ) { }
@@ -18,6 +19,19 @@ export class ProductService {
       'Authorization' : `Bearer ${token}`
     });
 
-    return this.http.get<any>(this.apiUrl, {headers});
+    return this.http.get<any>(this.apiUrl1, {headers});
+  }
+
+  getProductDetail(token: string, productId: string): Observable<any> {
+    const url = `${this.apiUrl2}/${productId}`;
+    const headers = new HttpHeaders({
+      'Authorization' : `Bearer ${token}`
+    });
+
+
+    return this.http.get<any>(url, {headers});
   }
 }
+
+
+//indexNum: number, pageSize: number, startDate: string, endDate: string
